0781139eb36ff71cb0403b9dd744e9e0b3a3c01a
myeghaneh <m.yeghaneh@fu-berlin.de>
2019-08-01 16:25:07 -0700

    PR Curve, dataset of RE, Map recension, DL result

--- ml/notebooks/.ipynb_checkpoints/utility_01-checkpoint.py
+++ ml/notebooks/.ipynb_checkpoints/utility_01-checkpoint.py
@@ -9,8 +9,8 @@ from sklearn.model_selection import cross_val_score, GridSearchCV
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import confusion_matrix,classification_report
 from sklearn.model_selection import KFold
-from sklearn.metrics import roc_curve  
-from sklearn.metrics import roc_auc_score 
+from sklearn.metrics import roc_curve, roc_auc_score , auc
+
 
 
 
@@ -26,7 +26,7 @@ def Color_Confusion_Matrix(y, y_test, y_pred):
     plt.savefig('CM_Test_01.png')
     
 
-def plot_roc_curve(fpr, tpr,auc):  
+def plot_roc_curve(fpr, tpr,AUC):  
     plt.plot(fpr, tpr, color='orange', label='ROC')
     plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
     plt.xlabel('False Positive Rate')
@@ -35,8 +35,17 @@ def plot_roc_curve(fpr, tpr,auc):
     plt.legend()
     plt.show()
     
+def plot_pr(recall,precision):
+    auc_score = auc(recall, precision)
+    plt.plot([0, 1], [0.5, 0.5], linestyle='--')
+    plt.plot(recall, precision, marker='.')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Recall vs Precision Curve with AUC {:.3f}'.format(auc_score))
+    plt.show()
+    print('AUC: %.3f' % auc_score)
+    
 def cross_validation_accuracy(clf,X,y,cv):
     scores = cross_val_score(clf, X, y, cv=cv)
     print(scores)
     print('Accuracy of : {:.3f} ± {:.3f}'.format(np.mean(scores), 2 * np.std(scores)))
-
